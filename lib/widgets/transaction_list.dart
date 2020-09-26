e1ef25b0b971996ce29af2a6219f7db673381780
shreyassingh431 <30365246+shreyassingh431@users.noreply.github.com>
2020-09-26 21:28:10 +0530

    SingleChildScrollView demo

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -7,49 +7,54 @@ class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
 
   TransactionList(this.transactions);
+
   @override
   Widget build(BuildContext context) {
-
-    return Column(
-      children: transactions.map((tx) {
-        return Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
-                ),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.purple, width: 2)),
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  '\$${tx.amount.toString()}',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    tx.title,
-                    style: TextStyle(
-                        fontSize: 16, fontWeight: FontWeight.bold),
+    return Container(
+        height: 300,
+        child: SingleChildScrollView(
+            child: Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.purple, width: 2)),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          '\$${tx.amount.toString()}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            //DateFormat('yyyy/MM/dd').format(tx.date),  // 1 way
+                            DateFormat.yMMMd().format(tx.date), //2nd way
+                            style: TextStyle(color: Colors.grey),
+                          )
+                        ],
+                      )
+                    ],
                   ),
-                  Text(
-                    //DateFormat('yyyy/MM/dd').format(tx.date),  // 1 way
-                    DateFormat.yMMMd().format(tx.date), //2nd way
-                    style: TextStyle(color: Colors.grey),
-                  )
-                ],
-              )
-            ],
-          ),
-        );
-      }).toList(),
+                );
+              }).toList(),
+            )
+        )
     );
   }
 }
