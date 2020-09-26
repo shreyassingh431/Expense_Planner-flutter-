07adf69b2c977580f45d1b257fa15d84330c3ccd
shreyassingh431 <30365246+shreyassingh431@users.noreply.github.com>
2020-09-26 22:13:48 +0530

    User Input and Output styling demo

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -8,6 +8,18 @@ class NewTransaction extends StatelessWidget {
 
   NewTransaction(this.addTx);
 
+  void submitData(){
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if(enteredTitle.isEmpty || enteredAmount<=0){
+      return;
+    }
+
+    addTx(enteredTitle, enteredAmount,);
+
+}
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -20,6 +32,7 @@ class NewTransaction extends StatelessWidget {
               TextField(
                 decoration: InputDecoration(labelText: 'Title'),
                 controller: titleController,
+                onSubmitted: (_)=> submitData(),
 //                    onChanged: (value){
 //                      titleInput = value;
 //                    },
@@ -27,14 +40,14 @@ class NewTransaction extends StatelessWidget {
               TextField(
                 decoration: InputDecoration(labelText: 'Amount'),
                 controller: amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_)=> submitData(),
                 //onChanged: (val) => amountInput = val,
               ),
               FlatButton(
                 child: Text('Add Transaction'),
                 textColor: Colors.purple,
-                onPressed: () {
-                  addTx(titleController.text, double.parse(amountController.text),);
-                },
+                onPressed: submitData,
               )
             ]),
       ),
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,50 +12,50 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         height: 300,
-            child: ListView.builder(
-              itemBuilder: (ctx, index){
-                return Card(
-                  child: Row(
+        child: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2)),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      '\$${transactions[index].amount.toStringAsFixed(2)}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.purple, width: 2)),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$${transactions[index].amount.toString()}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.purple),
-                        ),
+                      Text(
+                        transactions[index].title,
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            transactions[index].title,
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            //DateFormat('yyyy/MM/dd').format(tx.date),  // 1 way
-                            DateFormat.yMMMd().format(transactions[index].date), //2nd way
-                            style: TextStyle(color: Colors.grey),
-                          )
-                        ],
+                      Text(
+                        //DateFormat('yyyy/MM/dd').format(tx.date),  // 1 way
+                        DateFormat.yMMMd().format(transactions[index].date),
+                        //2nd way
+                        style: TextStyle(color: Colors.grey),
                       )
                     ],
-                  ),
-                );
-              }
+                  )
+                ],
+              ),
+            );
+          },
+          itemCount: transactions.length,
+        )
 
-            )
-
-        );
-
+    );
   }
 }
