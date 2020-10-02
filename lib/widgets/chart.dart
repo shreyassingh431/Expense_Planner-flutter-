712536d516efccd47128270513082e47d9ecd8b7
shreyassingh431 <30365246+shreyassingh431@users.noreply.github.com>
2020-10-02 22:22:50 +0530

    fittedbox, sizedBox, fractioanllysizedBox, ListTitle, circleavatar, (chart, baar, padding) get type method

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,4 +1,5 @@
 import 'package:expense_planner/models/Transaction.dart';
+import 'package:expense_planner/widgets/chart_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -7,36 +8,55 @@ class Chart extends StatelessWidget {
 
   Chart(this.recentTransactions);
 
-
-  List<Map<String, String>> get groupedTransactionValues {
-    return List.generate(7, (index) {  // second parameter is nothing but its taking method
-      final weekDay = DateTime.now().subtract(Duration(days: index),);
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      // second parameter is nothing but its taking method
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
       var totalSum = 0.0;
 
-      for(var i=0; i< recentTransactions.length; i++){
-        if(recentTransactions[i].date.day == weekDay.day && recentTransactions[i].date.month == weekDay.month &&
-            recentTransactions[i].date.year == weekDay.year){
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
           totalSum += recentTransactions[i].amount;
         }
-
       }
 
-      print(DateFormat.E().format(weekDay));
-      print(totalSum.toString());
-      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum.toString()};
+      // print(DateFormat.E().format(weekDay).substring(0,1));
+      // print(totalSum.toString());
+      return {
+        'day': DateFormat.E().format(weekDay),
+        'amount': totalSum
+      };
     });
   }
 
 
-  @override
-  Widget build(BuildContext context) {
-
-    return Card(
-      elevation: 6,
-      margin: EdgeInsets.all(20),
-      child: Row(children: <Widget>[
-      ],),
-    );
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + item['amount'];
+    });
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 6,
+        margin: EdgeInsets.all(20),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: groupedTransactionValues.map((data) {
+              return Flexible(
+                fit: FlexFit.tight,
+                child: ChartBar(data['day'], data['amount'],
+                    totalSpending ==0.0? 0.0 : (data['amount'] as double )/ totalSpending),
+              );
+            }).toList(),
+          ),
+        ));
+  }
 }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -17,15 +17,49 @@ class TransactionList extends StatelessWidget {
                 children: <Widget>[
                   Text(
                     'No transactions added yet',
-                    style: Theme.of(context).textTheme.title,),
-                  SizedBox(height: 10,),
-                  Container(height:200 ,child: Image.asset('assets/images/waiting.png', fit: BoxFit.cover,)),
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                      height: 200,
+                      child: Image.asset(
+                        'assets/images/waiting.png',
+                        fit: BoxFit.cover,
+                      )),
                 ],
               )
-            : ListView.builder(  // colon is of ternary operator
+            : ListView.builder(
+                // colon is of ternary operator
                 itemBuilder: (ctx, index) {
                   return Card(
-                    child: Row(
+                    elevation: 5,
+                    margin: EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 5,
+                    ),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        radius: 30,
+                        child: Padding(
+                          padding: EdgeInsets.all(6),
+                          child: FittedBox(
+                            child: Text('\$${transactions[index].amount}'),
+                          ),
+                        ),
+                      ),
+                      title: Text(
+                        transactions[index].title,
+                        style: Theme.of(context).textTheme.title,
+                      ),
+                      subtitle: Text(
+                        DateFormat.yMMMd().format(transactions[index].date),
+                      ),
+                    ),
+
+
+                    /* child: Row(
                       children: <Widget>[
                         Container(
                           margin: EdgeInsets.symmetric(
@@ -60,7 +94,7 @@ class TransactionList extends StatelessWidget {
                           ],
                         )
                       ],
-                    ),
+                    ),*/
                   );
                 },
                 itemCount: transactions.length,
